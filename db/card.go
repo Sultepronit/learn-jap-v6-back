ddfb0249a83ee8f0f2482db1698ac4da40c77c65
Sultepronit <sultepronit@gmail.com>
2026-03-15 19:53:57 +0200

    refactored sync; added card creation functionality

--- db/card.go
+++ db/card.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"japv6/models"
@@ -104,9 +105,51 @@ func UpdateCards(cards []models.Card, v int, table string, group string) (re []m
 	return re, v, tx.Commit()
 }
 
-func CreateCards(tableItem string, group string, cards []models.Card, v int) (re []models.CardMeta, newV int, err error) {
-	re = make([]models.CardMeta, len(cards))
+func selectMetaCardById(tx *sql.Tx, table string, group string, id int) (*models.CardMeta, error) {
+	query := fmt.Sprintf(`
+		SELECT %[1]s_v, %[1]s_sync_v
+		FROM %[2]s
+		WHERE id = ?
+	`, group, table)
+
+	var card models.CardMeta
+	err := tx.QueryRow(query, id).Scan(&card.V, &card.SyncV)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &card, nil
+}
+
+func updateCard(tx *sql.Tx, c models.Card, table string, group string) error {
+	query := fmt.Sprintf(`
+		UPDATE %[1]s
+		SET %[2]s_v = ?, %[2]s_sync_v = ?, %[2]s_data = ?
+		WHERE id = ?;
+	`, table, group)
+
+	_, err := tx.Exec(query, c.V, c.SyncV, c.Data, c.ID)
+	return err
+}
+
+func createCard(tx *sql.Tx, c models.Card, table string, group string) error {
+	query := fmt.Sprintf(`
+		INSERT INTO %[1]s (id, %[2]s_v, %[2]s_sync_v, %[2]s_data)
+		VALUES (?, ?, ?, ?);
+	`, table, group)
+
+	_, err := tx.Exec(query, c.ID, c.V, c.SyncV, c.Data)
+	return err
+}
+
+func UpsertCards(inputCards []models.Card, v int, tableEntry string, group string) (re []models.CardMeta, newV int, err error) {
+	re = make([]models.CardMeta, 0, len(inputCards))
 	newV = 0
+
+	isFresh := true
 
 	tx, err := conn.Begin()
 	if err != nil {
@@ -114,32 +157,36 @@ func CreateCards(tableItem string, group string, cards []models.Card, v int) (re
 	}
 	defer tx.Rollback()
 
-	query := fmt.Sprintf(`
-		INSERT INTO %[1]ss (id, %[2]s_v, %[2]s_sync_v, %[2]s_data)
-		VALUES (?, ?, ?, ?)
-		ON CONFLICT(id) DO UPDATE SET
-		%[2]s_v = excluded.%[2]s_v,
-		%[2]s_data = excluded.%[2]s_data
-		RETURNING %[2]s_sync_v;
-	`, tableItem, group)
-	// fmt.Println(query)
-	stmt, err := tx.Prepare(query)
-	if err != nil {
-		return
-	}
-	defer stmt.Close()
-
-	for i, c := range cards {
-		v++
-		c.SyncV = v
-		re[i] = c.CardMeta
-		_, err = stmt.Exec(c.V, c.SyncV, c.Data, c.ID)
+	table := tableEntry + "s"
+	for _, ic := range inputCards {
+		sc, err := selectMetaCardById(tx, table, group, ic.ID)
 		if err != nil {
-			return
+			return nil, 0, err
+		}
+		fmt.Println("sc:", sc)
+
+		var action func(*sql.Tx, models.Card, string, string) error
+
+		if sc == nil {
+			fmt.Println("new card!")
+			action = createCard
+		} else if ic.SyncV == sc.SyncV || (ic.V > sc.V && isFresh) || sc.SyncV < 1 {
+			action = updateCard
+		}
+
+		if action != nil {
+			v++
+			ic.SyncV = v
+			re = append(re, ic.CardMeta)
+			fmt.Println(ic.CardMeta)
+			err = action(tx, ic, table, group)
+			if err != nil {
+				return nil, 0, err
+			}
 		}
 	}
 
-	tn := fmt.Sprintf("%s_%ss", tableItem, group)
+	tn := fmt.Sprintf("%s_%ss", tableEntry, group)
 	_, err = tx.Exec("UPDATE versions SET val = ? WHERE id = ?", v, tn)
 	if err != nil {
 		return
--- sync/sync.go
+++ sync/sync.go
@@ -6,118 +6,57 @@ import (
 	"japv6/models"
 )
 
-func filterUpdates(table string, group string, inputC []models.Card, isFresh bool) ([]models.Card, error) {
-	ids := make([]int, len(inputC))
-	for i := range inputC {
-		ids[i] = inputC[i].ID
-	}
-
-	storedC, err := db.SelectMetaCardsByIds(table, group, ids)
-	if err != nil {
-		return nil, err
-	}
-
-	m := make(map[int]models.CardMeta)
-	for _, c := range storedC {
-		m[c.ID] = c
-	}
-	// fmt.Println("stored:", m)
-
-	re := make([]models.Card, 0, len(inputC))
-	for _, c := range inputC {
-		sc := m[c.ID]
-		// fmt.Println("syncV:", c.SyncV, sc.SyncV)
-		// fmt.Println("V:", c.V, sc.V)
-		if c.SyncV == sc.SyncV {
-			re = append(re, c)
-		} else if c.V > sc.V && isFresh {
-			re = append(re, c)
-		}
-	}
-
-	return re, nil
-}
-
-func update(table string, group string, updates []models.Card, lastV int, clientV int) ([]models.CardMeta, int, error) {
-	filtered, err := filterUpdates(table, group, updates, (clientV+100 > lastV))
-	if err != nil {
-		return nil, 0, err
-	}
-	// fmt.Println("filtered:", filtered)
-
-	if len(filtered) == 0 {
-		return nil, lastV, nil
-	}
-
-	return db.UpdateCards(filtered, lastV, table, group)
-}
-
 var typeToArgs = map[string][]string{
 	"wordCards": {"word", "card"},
 	"wordProgs": {"word", "prog"},
 }
 
-func sync(report models.Msg) (result *models.Msg, err error) {
-	result = &report
+func sync2(msg models.Msg) (*models.Msg, error) {
+	var res models.Msg
+	res.Type = msg.Type
 
-	args := typeToArgs[report.Type]
+	args := typeToArgs[msg.Type]
 
-	clientV := report.V
+	clientV := msg.V
 	lastV, err := db.GetVersion(fmt.Sprintf("%s_%ss", args[0], args[1]))
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	newV := lastV
-	if report.Updated != nil {
-		report.Accepted, newV, err = update(args[0], args[1], report.Updated, lastV, clientV)
+	if msg.Updated != nil {
+		res.Accepted, newV, err = db.UpsertCards(msg.Updated, lastV, args[0], args[1])
 		if err != nil {
 			return nil, err
 		}
-		// filtered, err := filterUpdates(args[0], args[1], report.Updated, (clientV + 100 > lastV))
-		// if err != nil {
-		// 	return nil, err
-		// }
-		// // fmt.Println("filtered:", filtered)
-
-		// if len(filtered) != 0 {
-		// 	report.Accepted, newV, err = db.UpdateCards(filtered, lastV, args[0], args[1])
-		// 	if err != nil {
-		// 		return nil, err
-		// 	}
-		// }
 	}
 
-	if report.Created != nil {
-		err = db.TempFillCards(report.Created, args[0], args[1])
-
-	}
-
-	if clientV == lastV && len(report.Accepted) == 0 {
+	if clientV == lastV && len(res.Accepted) == 0 {
 		return nil, nil
 	}
 	// fmt.Println(report.Accepted)
 
-	report.V = newV
-	report.Updated, err = db.SelectCardsSyncRange(args[0], args[1], clientV+1, lastV)
+	res.V = newV
+	res.Updated, err = db.SelectCardsSyncRange(args[0], args[1], clientV+1, lastV)
 	if err != nil {
-		return
+		return nil, err
 	}
 
-	return result, nil
+	return &res, nil
+	// return nil, nil
 }
 
-func Do(inputR []models.Msg) ([]*models.Msg, error) {
-	outputR := make([]*models.Msg, 0, len(inputR))
-	for _, r := range inputR {
-		rs, err := sync(r)
+func Do(inputMsg []models.Msg) ([]*models.Msg, error) {
+	outputMsg := make([]*models.Msg, 0, len(inputMsg))
+	for _, m := range inputMsg {
+		rs, err := sync2(m)
 		if err != nil {
 			return nil, err
 		}
 		if rs != nil {
-			outputR = append(outputR, rs)
+			outputMsg = append(outputMsg, rs)
 		}
 	}
 
-	return outputR, nil
+	return outputMsg, nil
 }
